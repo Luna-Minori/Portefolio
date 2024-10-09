fc0018fa4254bea1cb1425a7e27a05d13e394211
Luna-Minori <lunahascoet@gmail.com>
2024-10-09 23:51:33 +0200

    Beginning

--- Main.php
+++ Main.php
@@ -1,1 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Portefolio</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="Main.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <header>
+        <nav class="navbarMain">
+            <ul class="navul">
+                <li class="nav-item">
+                    <a  class="nav-lien" href="#me"> me </a>
+                </li>
+                <li class="nav-item">
+                    <a> Associatif</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#Stage"> Stage </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 
+</body>
+</html>
